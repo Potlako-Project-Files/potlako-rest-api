2616ac16db88cdb8deffefdf161f6a9a52115267
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-04-06 09:53:23 +0200

    django-cors-headers update

--- potlako_rest_api/serializers/navigation_summary_and_plan_serializers.py
+++ potlako_rest_api/serializers/navigation_summary_and_plan_serializers.py
@@ -6,4 +6,4 @@ from potlako_subject.models import NavigationSummaryAndPlan
 class NavigationSummaryAndPlanSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = NavigationSummaryAndPlan
-        fields = ('diagnostic_plan', 'notes')
+        fields = ('subject_identifier', 'diagnostic_plan', 'notes')
